b5e2b2b254869f6d19e0bf52a1a242a815f1dcb5
Salman Faris <farissalmannbr@gmail.com>
2024-12-18 23:47:43 +0530

    init doc

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>tinyML Hack Docs 📚</span>,
+  project: {
+    link: 'https://github.com/MakerGram/tinymlhack2024-docs',
+  },
+  docsRepositoryBase: 'https://github.com/MakerGram/tinymlhack2024-docs',
+  footer: {
+    text: 'tinyML Hackathon Docs',
+  },
+}
+
+export default config
