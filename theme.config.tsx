a73c909252f1cefbf99c869ace565a91f6c9abe9
Salman Faris <farissalmannbr@gmail.com>
2024-12-20 04:11:33 +0530

    added meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,10 +12,15 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
-      <title>tinyML Hack Docs 📚</title>
-      <meta name="description" content="Explore guides, examples, and resources for the TinyML Hackathon." />
+      <title>TinyML Hack Docs 📚</title>
+      <meta name="description" content="Explore guides, examples, and resources for the TinyML Hackathon 2024." />
+      <meta property="og:title" content="TinyML Hack Docs 📚" />
+      <meta property="og:description" content="Explore guides, examples, and resources for the TinyML Hackathon 2024." />
+      <meta property="og:url" content="https://tinymlhack.com/docs" />
+      <meta name="twitter:card" content="TinyML Hack Docs 📚" />
     </>
   ),
 }
 
 export default config
+
